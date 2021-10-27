ef0dca2103e2f84bf0af4ca0c4c1ccde7ae4126f
LuSrodri <lucassrodri08@gmail.com>
2021-10-27 02:11:10 -0300

    logic game part 1

--- script/game.js
+++ script/game.js
@@ -1,249 +1,225 @@
-const canvas = document.getElementById('game');
-const context = canvas.getContext('2d');
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 100;
+canvas.height = 200;
+
+ctx.fillStyle = "#2a2a30";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+document.getElementById("game").parentNode.replaceChild(canvas,document.getElementById("game"));
+
+let m = new Array();
+m = makeMatrix(10,20);
+console.log(m);
+
+let letters = ['T','Z','I','L','J','S','O'];
+let letter;
+let random = Math.floor(Math.random() * letters.length);
+let block = createBlock(letters[random]);
+letter = letters[random];
+putPiece(block);
+printGame(letter);
+
+setInterval(()=>{
+    let letters = ['T','Z','I','L','J','S','O'];
+    let teste = downPiece(letter);
+    if(teste){
+        printGame(letter);
+    }
+    else{
+        let random = Math.floor(Math.random() * letters.length);
+        let block = createBlock(letters[random]);
+        letter = letters[random];
+        putPiece(block);
+    }
 
 
-context.scale(20,20);
+},1000);
 
-function arenaSweep(){
-    let rowCount = 1;
-    outer: for (let y = arena.length - 1; y > 0 ; --y){
-        for(let x = 0; x < arena[y].length; ++x){
-            if(arena[y][x] === 0){
-                continue outer;
+document.body.addEventListener('keydown', function (event) {
+    const key = event.key;
+    if(key === "A" || key === "a"){
+        changeDirection('L');
+    }
+    if(key === "D" || key === "d"){
+        changeDirection('R');
+    }
+});
+
+function printGame(letter){ //print the game with the colors of the pieces
+    for(let x=0;x< m.length ;x++){
+        for(let y=0;y< m[x].length ;y++){
+            if(m[x][y] === 1){
+                ctx.fillStyle = colors(letter);
+                ctx.fillRect(y*10,x*10,(y+10),(x+10));
+            }
+            else if(m[x][y] === 0){
+                ctx.fillStyle = '#2a2a30';
+                ctx.fillRect(y*10,x*10,(y+10),(x+10));
+            }
+            else if(m[x][y] !== 1 && m[x][y] !== 0){
+                ctx.fillStyle = colors(m[x][y]);
+                ctx.fillRect(y*10,x*10,(y+10),(x+10));
             }
         }
-        const row = arena.splice(y,1)[0].fill(0);
-        arena.unshift(row);
-        ++y;
-
-        player.score += rowCount * 10;
-        rowCount += 1;
     }
-    
-    
 }
 
-
-function collide(arena,player){//colisão do game com o chão e peças
-    const [m, o] =[player.matrix,player.pos];
-    for(let y = 0; y < m.length;y++){
-        for(let x=0; x < m[y].length;x++){
-            if(m[y][x] !== 0 && 
-                (arena[y + o.y] &&
-                arena[y + o.y][x + o.x])!==0){
-                    return true;
-                }
+function downPiece(letter){ //move the piece down
+    let length = (m.length-1);
+    for(let x=length;x>=0;x--){
+        if((m[x].indexOf(1) !== -1 && x !== length)){  //if the piece is not in the ground or in on top of another piece
+            if(m[x+1].reduce(testIfUnderIsEmpty) === (m[x+1].length-1)){
+                m[x+1].fill(1,m[x].indexOf(1),(m[x].lastIndexOf(1)+1)); //if true, move the piece down
+                m[x].fill(0,m[x].indexOf(1),(m[x].lastIndexOf(1)+1));
+            }
+        }
+        if((m[x].indexOf(1) !== -1 && x === length) ){//if the piece is in the ground 
+               m[x].fill(letter,m[x].indexOf(1),(m[x].lastIndexOf(1)+1)); // if true, freeze the piece
+            }
+        
+        if((m[x].indexOf(1) !== -1 && x !== length) || //if the piece is in on top of another piece
+            (m[x].indexOf(1) !== -1 && m[x+1].reduce(testIfUnderIsEmpty) < (m[x+1].length-1) ) ){
+            mAux = m[x+1].slice(m[x+1].reduce(indexDifferent0),m[x+1].reduceRight(indexDifferent0));
+            if(mAux.length > 0){
+                m[x].fill(letter,m[x].indexOf(1),(m[x].lastIndexOf(1)+1)); // if true, freeze the piece
+            }
         }
     }
-    return false;
-};
 
-function createMAtrix(w,h){
-    const matrix = [];
-    while (h--){
-        matrix.push(new Array(w).fill(0));
+
+    let count = 0;
+    for(let x=length;x>=0;x--){
+        if(m[x].indexOf(1) === -1){ //check if exist a free piece in the matrix
+            count++;
+        }
+    }
+
+    if(count === m.length){
+        return false; //return false if there is no free piece in the matrix
+    }
+    else{
+        return true;  //return true if there is a free piece in the matrix
+    }
+} 
+
+testIfUnderIsEmpty = (acumulador, valorAtual, index, array) =>{
+    if(valorAtual === 0){
+        acumulador++;
+    }
+    return acumulador;
+}
+
+indexDifferent0 = (acumulador, valorAtual, index, array) =>{
+    if(valorAtual !== 0){
+        return index;
+    }
+}
+
+function putPiece(block){ //put the pieces in the matrix
+    
+    let count1 = 0;
+    for(let y=0;y<= 2;y++){
+        let count2 = 0;
+        for(let x=3;x<= 7;x++){
+            m[y][x] = block[count1][count2];
+            count2++;
+        }
+        count1++;
+    }
+
+}
+
+function changeDirection(direction){ //change the direction of the piece
+    for(let x=0;x< m.length ;x++){
+        for(let y=0;y< m[x].length ;y++){
+            if(m[x][y] === 1){
+                if(direction === "R"){
+                    m[x][y] = 0;
+                    m[x][y+1] = 1;
+                }
+                else if(direction === "L"){
+                    m[x][y] = 0;
+                    m[x][y-1] = 1;
+                }
+            }
+        }
+    }
+}
+
+function makeMatrix(width,height){ //make the matrix with the width and height for the game
+    let matrix = new Array();
+    for(let y=0;y< height;y++){
+        matrix[y] = new Array();
+        for(let x=0;x< width;x++){
+            matrix[y][x] = 0;
+        }
     }
     return matrix;
 }
 
-function createPiece(type){
-    if(type === 'T'){
-        return [
-            [0, 0, 0],
-            [1, 1, 1],
-            [0, 1, 0],
-        ];
-    } else if(type == 'O'){
-        return [
-            [2, 2],
-            [2, 2],
-        ];
-    } else if(type == 'L'){
-        return [
-            [0, 3, 0],
-            [0, 3, 0],
-            [0, 3, 3],
-        ];
-    } else if(type == 'J'){
-        return [
-            [0, 4, 0],
-            [0, 4, 0],
-            [4, 4, 0],
-        ];
-    } else if(type == 'I'){
-        return [
-            [0, 0, 0, 0],
-            [5, 5, 5, 5],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-        ];
-    } else if(type == 'S'){
-        return [
-            [0, 6, 6],
-            [6, 6, 0],
-            [0, 0, 0],
-        ];
-    } else if(type == 'Z'){
-        return [
-            [7, 7, 0],
-            [0, 7, 7],
-            [0, 0, 0],
-        ];
+function createBlock(op){//create the pieces by the option
+    if(op === 'T'){
+        return [[0,0,1,0,0],
+                [0,1,1,1,0],
+                [0,0,0,0,0]];  
     }
-
-}
-
-function desenhar(){//cria a arena do game
-    context.fillStyle = '#000';
-context.fillRect(0,0,canvas.clientWidth, canvas.height);
-    desenharMatrix(arena,{x:0,y:0});
-    desenharMatrix(player.matrix,player.pos);
-}
-
-
-function desenharMatrix(matrix, offset){//cria peça
-        matrix.forEach((linha,y)=>{
-            linha.forEach((valor, x)=>{
-                if(valor!== 0){
-                    context.fillStyle = colors[valor];
-                    context.fillRect(x + offset.x
-                                    ,y + offset.y,1,1)
-                }
-            });
-        });
-}
-
-function merge(arena,player){
-    player.matrix.forEach((linha,y)=>{
-        linha.forEach((value, x)=>{
-            if(value!== 0){
-                arena[y + player.pos.y][x + player.pos.x] = value;
-            }
-        })
-    })
-}
-
-function playerDrop(){
-    player.pos.y++;
-    if(collide(arena, player)){
-        player.pos.y--;
-        merge(arena,player);
-        playerReset();
-        arenaSweep();
-        updateScore();
+    if(op === 'Z'){
+        return [[0,1,1,0,0],
+                [0,0,1,1,0],
+                [0,0,0,0,0]];  
     }
-    dropCounter=0;
-}
-
-function playerMove(dir){//nao deixa passar para os lados
-    player.pos.x += dir;
-    if(collide(arena,player)){
-        player.pos.x -= dir;
+    if(op === 'I'){
+        return [[0,1,1,1,1],
+                [0,0,0,0,0],
+                [0,0,0,0,0]];  
+    }
+    if(op === 'L'){
+        return [[0,0,1,0,0],
+                [0,0,1,0,0],
+                [0,0,1,1,0]];  
+    }
+    if(op === 'J'){
+        return [[0,0,1,0,0],
+                [0,0,1,0,0],
+                [0,1,1,0,0]];  
+    }
+    if(op === 'S'){
+        return [[0,0,1,1,0],
+                [0,1,1,0,0],
+                [0,0,0,0,0]];  
+    }
+    if(op === 'O'){
+        return [[0,0,1,1,0],
+                [0,0,1,1,0],
+                [0,0,0,0,0]];  
     }
 }
 
-function playerReset(){
-    const pieces = 'ILJOTSZ';
-    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
-    player.pos.y = 0;
-    player.pos.x = (arena[0].length / 2 | 0) -
-                    (player.matrix[0].length / 2 | 0);
-    if(collide(arena, player)){
-        arena.forEach(row => row.fill(0));
-        player.score = 0;
-        updateScore();
+function colors(op){ //change the colors of the pieces
+    if(op === 'T'){
+        return 'red';
+    }
+    if(op === 'Z'){
+        return 'blue';
+    }
+    if(op === 'I'){
+        return 'green';
+    }
+    if(op === 'L'){
+        return 'yellow';
+    }
+    if(op === 'J'){
+        return 'orange';
+    }
+    if(op === 'S'){
+        return 'purple';
+    }
+    if(op === 'O'){
+        return 'white';
     }
 }
 
-function playerRotate(dir){//chama a funcao que ira rodar a peça, se for Q(-1) gira para a esquerda e W(1) gira para a direita
-    const pos = player.pos.x;
-    let offset = 1;
-    rotate(player.matrix, dir)
-    while (collide(arena, player)){
-        player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].length){
-            rotate(player.matrix, -dir);
-            player.pos.x=pos;
-            return;
-        }
-    }
-}
-function rotate(matrix,dir){//gira as pecas baseado na direcao
-        for(let y = 0; y < matrix.length; ++y){
-            for(let x = 0; x<y; ++x){
-                [
-                    matrix[x][y],
-                    matrix[y][x],
-                ] =[
-                    matrix[y][x],
-                    matrix[x][y],
-
-                ];
-            }
-        }
-        if(dir > 0){
-            matrix.forEach(row => row.reverse());
-        } else{
-            matrix.reverse();
-        }
-}
-let dropCounter = 0;
-let dropInterval = 1000;
-
-let lastTime =0;
-function update(time = 0){
-    const deltaTime = time - lastTime;
-    lastTime = time;
-    dropCounter += deltaTime;
-
-    
-    if(dropCounter > dropInterval){
-        playerDrop();
-    }
 
 
-    desenhar();
-    requestAnimationFrame(update)
-}
-
-function updateScore() {
-    document.getElementById('score').innerText = player.score;
-}
-
-const colors = [
-    null,
-    'red',
-    'blue',
-    'green',
-    'yellow',
-    'orange',
-    'violet',
-    'cyan',
-]
-
-const arena = createMAtrix(12,20);
-const player = {
-    pos:{x:0,y:0},
-    matrix: null,
-    score: 0,
-}
-
-document.addEventListener('keydown', event =>{//move com as setas e gira com Q e W 
-if(event.keyCode === 37){//seta para a esquerda
-    playerMove(-1);
-} else if(event.keyCode === 39){//seta para a direita
-    playerMove(1);
-}else if(event.keyCode === 40){//Seta para baixo
-    playerDrop();
-}else if(event.keyCode === 81){//Q
-    playerRotate(-1);
-}else if(event.keyCode === 87){//W
-    playerRotate(1);
-}
-})
-
-playerReset();
-updateScore();
-update();
 
